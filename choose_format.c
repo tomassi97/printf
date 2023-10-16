71987a20eeb1e9ee19b48818671de7e4a974136d
tomassi97 <thomasmithamo99@gmail.com>
2023-10-16 02:21:18 -0700

    Function chooses the format

---
+++ choose_format.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * choose_format - Function chooses format of the argument
+ * passed to it
+ * @args : list of the variables argments
+ * @type : format of argument
+ * Return: number of char printed
+ */
+int choose_format(va_list args, char type)
+{
+	int count = 0;
+
+	if (type == 'c')
+	{
+		_putchar(va_arg(args, int));
+		count = count + 1;
+	}
+	return (count);
+}
--- main.h
+++ main.h
@@ -4,4 +4,5 @@
 #include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
+int choose_format(va_list args, char type);
 #endif
